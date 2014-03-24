fc7eaa7ceb9182b51ec5cf897cc67d40ab67b915
zikai88 <zi.kai88@gmail.com>
2014-03-24 19:09:53 +0800

    Added window form for first page setting

---
+++ AMDES_KBS/AMDES_KBS/frmFirstPageSetting.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AMDES_KBS.Controllers;
+using AMDES_KBS.Entity;
+
+namespace AMDES_KBS
+{
+    /// <summary>
+    /// Interaction logic for frmFirstPageSetting.xaml
+    /// </summary>
+    public partial class frmFirstPageSetting : Window
+    {
+        public frmFirstPageSetting()
+        {
+            InitializeComponent();
+            loadAllSection();
+        }
+
+        private void loadAllSection()
+        {
+            cboSectionList.ItemsSource = QuestionController.getAllQuestionGroup();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+
+        private void saveFirstSection()
+        {
+            //Update first navigation link
+        }
+    }
+}
